1412aa6e760a8da3f06a0f1320233de3485cce0e
Robert Jan Zwetsloot <zwetsloot.robert.jan@techcross.co.jp>
2017-04-17 09:31:29 +0900

    changed to_existing_atom to to_atom, as the atoms of the modules being build potentially do not exist yet

--- lib/mythcraftMasterData.ex
+++ lib/mythcraftMasterData.ex
@@ -1,14 +1,11 @@
 defmodule MasterData do
-  require MasterData.BloodRush
-  require MasterData.LostShrine
-  require MasterData.GrimStraits
 
   def get_maplist() do
     Maplist.get
   end
 
   def get_gamedata(map) do
-    module_name = String.to_existing_atom("Elixir.MasterData." <> String.replace(map, ~r/ /, ""))
+    module_name = String.to_atom("Elixir.MasterData." <> String.replace(map, ~r/ /, ""))
     apply(module_name, :get, [])
   end
 
